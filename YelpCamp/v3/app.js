f9883b8366855e0e25b48b451106cba030b89256
hashmat MH <hashmat00@aol.com>
2016-03-02 22:08:00 +0000

    added comment on Yelp v3

---
+++ YelpCamp/v3/app.js
@@ -0,0 +1,80 @@
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    Campground = require("./models/campground"),
+    seedDB = require("./seeds")
+   
+    
+seedDB();
+    
+
+mongoose.connect("mongodb://localhost/yelp_camp_v3");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+
+
+
+
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res) {
+        //Get all campgrounds from DB
+        Campground.find({}, function(err, allCampgrounds){
+            if(err){
+                console.log(err);
+                
+            }else{
+               res.render("index", {campgrounds: allCampgrounds}); 
+            }
+        });
+    
+});
+
+
+app.post("/campgrounds", function(req, res){
+    
+    //get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description: desc};
+    //Crearte a new campground  and save to DB
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        }else{
+          //redirect to campgrouds page
+    res.redirect("/campgrounds");  
+        }
+    });
+    
+});
+
+
+app.get("/campgrounds/new", function(req, res) {
+    res.render("new.ejs");
+});
+
+  //SHOW - shows more info about one campground
+app.get("/campgrounds/:id",  function(req, res) {
+    //find teh campground with provided ID
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+        if(err){
+            console.log(err);
+        }else{
+            console.log(foundCampground);
+            //render show template with that campground
+            res.render("show", {campground: foundCampground});
+        }
+    });
+});
+
+//start the server
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("YelpCamp Server has started");
+});
