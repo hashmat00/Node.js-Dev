08c8b2c97aac1e6b751b39ea7d518c71c528f9a5
hashmat MH <hashmat00@aol.com>
2016-03-03 04:53:54 +0000

    v5 added

---
+++ YelpCamp/v5/app.js
@@ -0,0 +1,122 @@
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    Campground = require("./models/campground"),
+     Comment     = require("./models/comment"),
+    seedDB = require("./seeds")
+   
+
+    
+
+mongoose.connect("mongodb://localhost/yelp_camp_v3");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+seedDB();
+app.use(express.static(__dirname + "/public"));
+
+
+
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res) {
+        //Get all campgrounds from DB
+        Campground.find({}, function(err, allCampgrounds){
+            if(err){
+                console.log(err);
+                
+            }else{
+               res.render("campgrounds/index", {campgrounds: allCampgrounds}); 
+            }
+        });
+    
+});
+
+
+app.post("/campgrounds", function(req, res){
+    
+    //get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description: desc};
+    //Crearte a new campground  and save to DB
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        }else{
+          //redirect to campgrouds page
+    res.redirect("/campgrounds");  
+        }
+    });
+    
+});
+
+
+app.get("/campgrounds/new", function(req, res) {
+    res.render("campgrounds/new.ejs");
+});
+
+  //SHOW - shows more info about one campground
+app.get("/campgrounds/:id",  function(req, res) {
+    //find teh campground with provided ID
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+        if(err){
+            console.log(err);
+        }else{
+            console.log(foundCampground);
+            //render show template with that campground
+            res.render("campgrounds/show", {campground: foundCampground});
+        }
+    });
+});
+
+
+
+// ====================
+// COMMENTS ROUTES
+// ====================
+
+app.get("/campgrounds/:id/comments/new", function(req, res){
+    // find campground by id
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        } else {
+             res.render("comments/new", {campground: campground});
+        }
+    })
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+   //lookup campground using ID
+   Campground.findById(req.params.id, function(err, campground){
+       if(err){
+           console.log(err);
+           res.redirect("/campgrounds");
+       } else {
+        Comment.create(req.body.comment, function(err, comment){
+           if(err){
+               console.log(err);
+           } else {
+               campground.comments.push(comment);
+               campground.save();
+               res.redirect('/campgrounds/' + campground._id);
+           }
+        });
+       }
+   });
+   //create new comment
+   //connect new comment to campground
+   //redirect campground show page
+});
+
+
+
+//start the server
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("YelpCamp Server has started");
+});
--- YelpCamp/v5/seeds.js
+++ YelpCamp/v5/seeds.js
@@ -6,25 +6,23 @@ var data = [
     {
         name: "Cloud's Rest", 
         image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
-        description: "blah blah blah"
+        description: "Bold, tenacious and pushing colossal sound, this is FUGOO Sport XL. Featuring 8 acoustic drivers for true 360 sound, waterproof and dust proof with 35 hour battery life and a USB port to quick charge your phone or tablet. It even floats in water for easy recovery making it an ideal active speaker around the pool, lake or ocean. Unique to FUGOO Sport XL, the 6 control buttons on top glow in the dark so even on a night camping, you’re still in control."
     },
     {
         name: "Desert Mesa", 
         image: "https://farm4.staticflickr.com/3859/15123592300_6eecab209b.jpg",
-        description: "blah blah blah"
+        description: "Bold, tenacious and pushing colossal sound, this is FUGOO Sport XL. Featuring 8 acoustic drivers for true 360 sound, waterproof and dust proof with 35 hour battery life and a USB port to quick charge your phone or tablet. It even floats in water for easy recovery making it an ideal active speaker around the pool, lake or ocean. Unique to FUGOO Sport XL, the 6 control buttons on top glow in the dark so even on a night camping, you’re still in control."
     },
     {
         name: "Canyon Floor", 
         image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
-        description: "blah blah blah"
+        description: "Bold, tenacious and pushing colossal sound, this is FUGOO Sport XL. Featuring 8 acoustic drivers for true 360 sound, waterproof and dust proof with 35 hour battery life and a USB port to quick charge your phone or tablet. It even floats in water for easy recovery making it an ideal active speaker around the pool, lake or ocean. Unique to FUGOO Sport XL, the 6 control buttons on top glow in the dark so even on a night camping, you’re still in control."
     }
 ]
 
-
-
 function seedDB(){
-  //Remove all campgrounds
-  Campground.remove({}, function(err){
+   //Remove all campgrounds
+   Campground.remove({}, function(err){
         if(err){
             console.log(err);
         }
